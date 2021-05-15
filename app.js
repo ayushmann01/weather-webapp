e0a0be7d3bc82bff66726914953aa416cfbdec73
Ayushman <ayushmanraghav.ra@gmail.com>
2021-05-15 15:45:17 +0530

    fix port

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.post("/", (request, res) => {
 
 
 
-app.listen(process.env.PORT |  3000, function () {
+app.listen(process.env.PORT ||  3000, function () {
     console.log("weather app started at port-3000");
 });
